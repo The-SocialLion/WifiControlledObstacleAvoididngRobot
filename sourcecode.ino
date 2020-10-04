d468d376e795d9bbacda1a7bf53c9fda357052eb
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-10-04 15:49:49 +0530

    created sourcecode

---
+++ sourcecode.ino
@@ -0,0 +1,200 @@
+/* include library */
+#include <ESP8266WiFi.h>
+#include <NewPingESP8266.h>
+#include <Servo.h>
+/* define port */
+WiFiClient client;
+WiFiServer server(80);
+/* WIFI settings */
+const char* ssid = "vishsid";
+const char* password = "Clearwater@543";
+/* data received from application */
+String  data =""; 
+/* define L298N or L293D motor control pins */
+int leftMotorForward = 2;     /* GPIO2(D4) -> IN3   */
+int rightMotorForward = 15;   /* GPIO15(D8) -> IN1  */
+int leftMotorBackward = 0;    /* GPIO0(D3) -> IN4   */
+int rightMotorBackward = 13;  /* GPIO13(D7) -> IN2  */
+/* define L298N or L293D enable pins */
+int rightMotorENB = 14; /* GPIO14(D5) -> Motor-A Enable */
+int leftMotorENB = 12;  /* GPIO12(D6) -> Motor-B Enable */
+int  trig_pin = 5;
+int echo_pin  = 6;
+#define maximum_distance 200
+boolean goesForward = false;
+int distance = 100;
+NewPingESP8266 sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+Servo servo_motor; // servo library object
+
+void setup()
+{
+  /* initialize motor control pins as output */
+  pinMode(leftMotorForward, OUTPUT);
+  pinMode(rightMotorForward, OUTPUT); 
+  pinMode(leftMotorBackward, OUTPUT);  
+  pinMode(rightMotorBackward, OUTPUT);
+
+  /* initialize motor enable pins as output */
+  pinMode(leftMotorENB, OUTPUT); 
+  pinMode(rightMotorENB, OUTPUT);
+  servo_motor.attach(7); 
+
+  servo_motor.write(115);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+
+  /* start server communication */
+  server.begin();
+}
+
+void loop()
+{
+    /* If the server available, run the "checkClient" function */  
+    client = server.available();
+    if (!client) return; 
+    data = checkClient ();
+
+/************************ Run function according to incoming data from application *************************/
+
+    /* If the incoming data is "forward", run the "MotorForward" function */
+    if (data == "forward") MotorForward();
+    /* If the incoming data is "backward", run the "MotorBackward" function */
+    else if (data == "backward") MotorBackward();
+    /* If the incoming data is "left", run the "TurnLeft" function */
+    else if (data == "left") TurnLeft();
+    /* If the incoming data is "right", run the "TurnRight" function */
+    else if (data == "right") TurnRight();
+    /* If the incoming data is "stop", run the "MotorStop" function */
+    else if (data == "stop") MotorStop();
+    else if (data =="OAR") OAR();
+} 
+
+/********************************************* FORWARD *****************************************************/
+void MotorForward(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+}
+
+/********************************************* BACKWARD *****************************************************/
+void MotorBackward(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorBackward,HIGH);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+}
+
+/********************************************* TURN LEFT *****************************************************/
+void TurnLeft(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH); 
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(rightMotorBackward,LOW);
+  digitalWrite(leftMotorBackward,HIGH);  
+}
+
+/********************************************* TURN RIGHT *****************************************************/
+void TurnRight(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+}
+
+/********************************************* STOP *****************************************************/
+void MotorStop(void)   
+{
+  digitalWrite(leftMotorENB,LOW);
+  digitalWrite(rightMotorENB,LOW);
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+}
+int lookRight(){  
+  servo_motor.write(50);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+}
+int lookLeft(){
+  servo_motor.write(170);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+  delay(100);
+}
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
+
+void OAR(void){
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+  if (distance <= 20){
+    MotorStop();
+    delay(300);
+    MotorBackward();
+    delay(400);
+    MotorStop();
+    delay(300);
+    distanceRight = lookRight();
+    delay(300);
+    distanceLeft = lookLeft();
+    delay(300);
+
+    if (distance >= distanceLeft){
+      TurnRight();
+      MotorStop();
+    }
+    else{
+      TurnLeft();
+      MotorStop();
+    }
+  }
+  else{
+   MotorForward(); 
+  }
+    distance = readPing();
+}
+ 
+
+
+/********************************** RECEIVE DATA FROM the APP ******************************************/
+String checkClient (void)
+{
+  while(!client.available()) delay(1); 
+  String request = client.readStringUntil('\r');
+  request.remove(0, 5);
+  request.remove(request.length()-9,9);
+  return request;
+}
